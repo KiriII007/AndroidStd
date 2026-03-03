243ada7e6fede48a78debbe92c97a2724646e499
KiriII007 <104263275+KiriII007@users.noreply.github.com>
2026-03-04 00:22:29 +0300

    Work1
    Первая работа

---
+++ work1/phonetabletmodule/src/main/java/com/mirea/kyi/phonetabletmodule/MainActivity.java
@@ -0,0 +1,53 @@
+package com.mirea.kyi.phonetabletmodule;
+
+import android.os.Bundle;import android.view.View;import android.widget.Button;import android.widget.CheckBox;import android.widget.TextView;import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView textViewStudent;
+    private Button btnWhoAmI;
+    private Button btnItIsNotMe ;
+
+    public CheckBox checkBox;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        textViewStudent = findViewById(R.id.textViewStudent1);
+        btnWhoAmI = findViewById(R.id.btnWhoAmI);
+        btnItIsNotMe = findViewById(R.id.btnItIsNotMe);
+        checkBox = findViewById(R.id.checkbox12);
+
+        View.OnClickListener oclBtnWhoAmI = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                textViewStudent.setText("Мой номер по списку № 25");
+                checkBox.setChecked(true);
+            }
+        };
+
+        btnWhoAmI.setOnClickListener(oclBtnWhoAmI);
+    }
+
+
+    public void onMyButtonClick(View view)
+    {
+        // выводим сообщение
+        btnItIsNotMe.setText("Это не я сделал");
+        checkBox.setChecked(false);
+    }
+}
